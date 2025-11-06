3ec0c3a35895f199dcf0d30f8bb1d76c1af280de
bastiennoel93 <bastiennoel93@gmail.com>
2025-11-06 16:26:56 +0100

    feat: azure static

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,15 +1,10 @@
 import { getRequestConfig } from 'next-intl/server';
-import { cookies } from 'next/headers';
-import { defaultLocale, type Locale, locales } from './config';
+import { defaultLocale } from './config';
 
 export default getRequestConfig(async () => {
-  const cookieStore = await cookies();
-  const localeCookie = cookieStore.get('NEXT_LOCALE')?.value as Locale | undefined;
-
-  // Use cookie locale if valid, otherwise use default
-  const locale = localeCookie && locales.includes(localeCookie)
-    ? localeCookie
-    : defaultLocale;
+  // For static export, we use the default locale
+  // Locale switching will be handled client-side
+  const locale = defaultLocale;
 
   return {
     locale,
