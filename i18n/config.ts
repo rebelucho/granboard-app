e8b1a549dfb5599e759c72fc5c77898711b4378a
Dmitry Kondratenko <rebelucho@gmail.com>
2026-01-07 00:17:51 +0900

    default language EN

--- i18n/config.ts
+++ i18n/config.ts
@@ -1,7 +1,7 @@
 export const locales = ['fr', 'en', 'ru'] as const;
 export type Locale = (typeof locales)[number];
 
-export const defaultLocale: Locale = 'ru';
+export const defaultLocale: Locale = 'en';
 
 export const localeNames: Record<Locale, string> = {
   fr: 'Français',
